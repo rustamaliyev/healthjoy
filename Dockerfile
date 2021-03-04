37cd4c8ca7e14543a23fe63ecfab52eb91a34ff4
rustam <rustam_wu@yahoo.com>
2021-03-03 21:50:08 -0600

    finished solution, added docker file to containerize the app

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.8
+
+LABEL Rustam Aliyev "rustam85@gmail.com"
+
+RUN apt-get update -y && \
+    apt-get install -y python3-pip python3-dev
+
+COPY ./requirements.txt /requirements.txt
+
+WORKDIR /
+
+RUN pip3 install -r requirements.txt
+
+COPY . /
+
+ENTRYPOINT [ "python3" ]
+
+CMD [ "app.py" ]
--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+from github import Github
+
 app = Flask(__name__)
+repo_user = Github("416434f53842f0720f24b38591b6dd3bcb0bdfb0")
 
+@app.route('/',methods = ['GET'])
+def index():
+    forks = repo_user.get_user().get_repo('healthjoy').get_forks()
+    repo = repo_user.get_user().get_repo('healthjoy')
+    return render_template("index.html", repo=repo, forks=forks)
 
-@app.route('/')
-def home():
-    return 'Welcome to Web Service!'
+@app.route('/fork',methods=['GET', 'POST'])
+def fork():
+  if request.method == 'POST':
+    repo = repo_user.get_user().get_repo('healthjoy')
+    github_pat = request.form["github_pat"]
+    github_user = Github(github_pat)
+    user = github_user.get_user()
+    myfork = user.create_fork(repo)
+    return render_template("success.html", user=user,repo=repo)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
